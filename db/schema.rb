f154a1a4f784af4750a7b3c573dcaac42dbf1e9d
Joel Torres <joel@coldmusicgroup.com>
2019-01-17 19:45:21 -0800

    created seeds file for sample data

--- db/schema.rb
+++ db/schema.rb
@@ -17,6 +17,7 @@ ActiveRecord::Schema.define(version: 2019_01_17_230619) do
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
+    t.string "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,32 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do |blog|
+  Blog.create!(
+     title: "my blog post #{blog}",
+     body: "Bof human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is p
+     leasure, but because those who not know how to pursue pleasure rationally encounter consequences that are extremely painf
+     obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain ca
+     kes laborious physical exercise, except to obtain some advantage from it? But who has any right to fi?"
+  )
+end
+
+puts "10 blog posts created"
+
+5.times do |skill|
+  Skill.create!(
+    title: "Rails #{skill}",
+    percent_utulize: 15
+  )
+end
+
+puts "5 skills posts created"
+
+9.times do |portfolio_item|
+  Portfolio.create!(
+    title: "Portfolio title: #{portfolio_item}",
+    subtitle: "My Great Artist",
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor",
+    main_image: "https://place-hold.it/600x400/red/white&text=ruby%20on%20rails",
+    thumb_image: "https://place-hold.it/350x200/red/white&text=ruby-rails",
+  )
+end
+puts "9 portolio posts created"
